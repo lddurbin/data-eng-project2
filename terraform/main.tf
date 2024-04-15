8f0a54d92237937016871f2f3163938a5d33816b
lddurbin <l.d.durbin@gmail.com>
2024-04-15 13:00:50 +1200

    Setting up Terraform stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,25 @@ terraform {
 }
 
 provider "google" {
-  project = "bigquerylearning-402002"
+  project = var.project
+  zone = var.zone
+  region = var.region
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags = ["zoomcamp", "r"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project" { }
+
+variable "region" {
+  description = "Project Region"
+  default     = "asia"
+}
+
+variable "zone" {
+  description = "Zone"
+  default     = "australia-southeast2-a"
+}
