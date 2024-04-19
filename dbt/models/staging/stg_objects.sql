bfa640e948ac69ea80c2e78521e20f62b8c161a3
Lee Durbin <l.d.durbin@gmail.com>
2024-04-19 03:48:09 +0000

    Staging model complete

---
+++ dbt/models/staging/stg_objects.sql
@@ -0,0 +1,29 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with source as (
+    select * from {{ source('staging','met_objects') }}
+),
+
+renamed as (
+    select
+    {{ dbt.safe_cast("object_id", api.Column.translate_type("string")) }} as id,
+    department,
+    accession_year,
+    culture,
+    classification
+
+    from source
+)
+
+select * from renamed
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
